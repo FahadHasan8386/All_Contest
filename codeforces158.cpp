102aa7afea5d40febdedcdcd3489d7d74cf4d5ea
fahadhasan <hasan.fahad67@outlook.com>
2024-01-29 01:15:59 +0600

    vector array problem

---
+++ codeforces158.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+    int n , k , s;
+
+    int next = 0;
+
+    cin >> n >> k;
+    vector <int> v ;
+
+    for (int i = 0 ; i < n ; i++){
+
+        cin >> s;
+        v.push_back(s);
+
+    }
+
+    for (int i = 0 ; i < n ; i++){
+
+        if ( v[i] >= v[k-1] ; v[i] > 0 ){
+
+            next++;
+        }
+
+    }
+    cout << next <<endl;
+
+    return 0;
+}
+
---
+++ vactor.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main (){
+
+    int a [4];
+
+    vector <int> v;
+
+    v.push_back (4);
+    v.push_back (5);
+    v.push_back(6);
+
+
+    cout << v[2] << endl;
+    cout << v.size() << endl;
+
+    vector <int> p;
+    cin
+
+
+}
