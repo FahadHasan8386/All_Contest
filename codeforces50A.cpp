f359fffaf893c707189b802b77669d96602589ee
fahadhasan <hasan.fahad67@outlook.com>
2024-02-01 01:33:44 +0600

    Domino piling CodeForces

---
+++ codeforces50A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+    int n , m ;
+    int domino = 2 ;
+
+        cin >> n >> m;
+
+    int temp = n * m ;
+
+    int domino_piling = temp / domino;
+
+        cout << domino_piling << endl;
+
+}
