1fe6a649a376ffdb532e7f3a2d3bebc4328e0a18
fahadhasan <hasan.fahad67@outlook.com>
2024-02-18 21:20:43 +0600

    Pointer

--- pointer.cpp
+++ pointer.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 
@@ -6,8 +7,27 @@ int main ()
     int x = 5 ;
     int *p = &x;
 
-        cout << x << endl;
-        cout << &x << endl;
-        cout << p << endl;
-        cout << *p << endl;
+        cout << x << endl;//value of x
+        cout << &x << endl;//address of x
+        cout << p << endl;//address of x
+        cout << *p << endl;//value of x
+}
+*/
+#include <iostream>
+using namespace std ;
+int main ()
+{
+    int num1 = 10;
+    int num2 = 20;
+
+    int *p1 ,*p2;
+    p1 = &num1;
+    p2 = &num2;
+    //value of num1 and num2
+    int sum = *p1 + *p2;
+
+    cout << sum << endl;
+    cout << p1 << endl;
+
+
 }
