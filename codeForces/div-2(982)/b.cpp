39a2155ae94bd8e36e87a49d9afa7db89254b360
Fahad Hasan <hasan.fahad67@outlook.com>
2024-10-27 17:44:03 +0600

    Contest Problem

--- codeForces/div-2(982)/b.cpp
+++ codeForces/div-2(982)/b.cpp
@@ -1,30 +1,37 @@
 #include <bits/stdc++.h>
-using namespace std ;
+using namespace std;
 
-int main ()
+int main()
 {
     int t;
     cin >> t;
-    int cnt = 0 ;
     while(t--)
     {
-        int  n;
+        int n;
         cin >> n;
 
-        vector<int>v(n);
-
-        for(int i = 0 ; i < n ;i++)
+        vector<int> v(n);
+        for(int i = 0; i < n; i++)
         {
             cin >> v[i];
         }
-        for(int i = 1 ; i < n-1 ;i++)
+
+        int ans = n;
+
+        for(int i = 0; i < n; i++)
         {
-            if(v[i-1] < v[i])
+            int cnt = 0;
+            for(int j = i + 1; j < n; j++)
             {
-                v.erase(v.begin());
-                cnt++;
+                if(v[j] > v[i])
+                {
+                    cnt++;
+                }
             }
+            ans = min(ans, cnt+i);
         }
-        cout << cnt << endl;
+
+        cout << ans << endl;
     }
+    return 0;
 }
